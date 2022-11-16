17791a78ebdf15d6f66177e97128fde7e695b4ac
pexmee <pexmee@gmail.com>
2022-11-16 19:41:13 +0900

    random commit, starting over with this stuff

--- assignment_1/headers/traverser.hpp
+++ assignment_1/headers/traverser.hpp
@@ -17,11 +17,13 @@
 #include <fstream>
 #include <time.h>
 #include <random>
+#include <ctime>
 
 /* Defines */
-#define DATA128 "data128.txt"
-#define DATA16K "data16k.txt"
-#define CYCLE_FILE "cycles_16k.ndjson"
+#define DATA128 "../data128.txt"
+#define DATA16K "../data16k.txt"
+#define CYCLE_FILE_16K "cycles_16k.ndjson"
+#define CYCLE_FILE_128 "cycles_128.njdson"
 #define MAX_X
 #define MAX_Y
 #define INT_MAX 2147483647
@@ -39,16 +41,11 @@ class Traverser{
         std::vector<std::vector<Node>> new_generation;
         std::vector<Node> possible_starting_nodes;
         std::vector<double> fitness_scores;
-        Node* current_node;
-        double distance_traversed;
-        bool starting_nodes_set;
         
     public:
         Traverser();
         ~Traverser();
-        void graph();
         void initialize_nodes();
-        void close_cycle();
         double calc_dist(Node &node);
         void dump_cycle(double fitness, std::vector<Node> &cycle);
         void mutate();
@@ -56,8 +53,6 @@ class Traverser{
         void randomize_parents();
         std::vector<std::vector<std::vector<Node>>> select_parents(); // This is obviously stupid but I shall fix this later on.
         void get_data(); // Need to be modified a bit from python version
-        std::vector<std::pair<size_t,size_t>> calc_clusters_for_threads();
-        Node pop_possible_starting_nodes();
         void genetic_algorithm_driver();
         std::vector<std::vector<std::vector<Node>>> select_parents(std::vector<std::vector<Node>> &population);
         void calculate_all_fitness(std::vector<std::vector<Node>> &population);
--- assignment_1/src/traverser.cpp
+++ assignment_1/src/traverser.cpp
@@ -1,7 +1,7 @@
 #include "../headers/traverser.hpp"
 
-double dist_calc(Node &current_node, Node &neighbor){
-    return std::sqrt(std::pow((neighbor.x - current_node.x),2) + std::pow((neighbor.y - current_node.y),2));
+__always_inline double dist_calc(Node &current_node, Node &neighbor){
+    return std::sqrt(std::pow((current_node.x - neighbor.x),2) + std::pow((current_node.y - neighbor.y),2));
 }
 
 size_t NUM_THREADS = 0;
@@ -34,16 +34,11 @@ Traverser::Traverser(){
     this->new_generation = {};
     this->cycle = {};
     this->possible_starting_nodes = {};
-    this->current_node = nullptr;
-    this->distance_traversed = 0;
-    this->starting_nodes_set = false;
     this->fitness_scores = {};
 }
 
 Traverser::~Traverser(){   
-    if(this->current_node != nullptr){
-        delete this->current_node;
-    }
+    // do absolutely nothing
 }
 
 
@@ -51,11 +46,12 @@ std::vector<double> calc_probabilities(std::vector<double> &fitness_scores){
     double total_fitness = 0;
     std::vector<double> probabilities = {};
 
-    for(unsigned i = 0; i < fitness_scores.size(); i++){
+    for(size_t i = 0; i < fitness_scores.size(); i++){
         total_fitness += fitness_scores[i];
     }
-    for(unsigned i = 0; i < fitness_scores.size(); i++){
+    for(size_t i = 0; i < fitness_scores.size(); i++){
         probabilities.push_back(fitness_scores[i] / total_fitness);
+        std::cout << "fitness score: " << fitness_scores[i] / total_fitness << "\n";
     }
     return probabilities;
 }
@@ -65,7 +61,7 @@ std::vector<std::vector<std::vector<Node>>> Traverser::select_parents(std::vecto
     std::vector<double> probabilities = calc_probabilities(this->fitness_scores);
     std::discrete_distribution<std::size_t> d{probabilities.begin(), probabilities.end()};
     std::vector<std::vector<Node>> parents_a, parents_b;
-    for(unsigned i = 0; i < probabilities.size(); i++){
+    for(size_t i = 0; i < probabilities.size(); i++){
         parents_a.push_back(population[d(gen)]);
         parents_b.push_back(population[d(gen)]);
     }
@@ -75,12 +71,13 @@ std::vector<std::vector<std::vector<Node>>> Traverser::select_parents(std::vecto
 }
 
 std::vector<Node> make_child(std::vector<Node> &parent_a, std::vector<Node> &parent_b){
-    std::vector<Node> child = {};
-    for(unsigned i = 0; i< parent_a.size()/3; i++){
+    std::vector<Node> child;
+    // std::copy(parent_a.begin(), parent_a.begin() + int(parent_a.size()/3),chi    ld.begin());
+    for(size_t i = 0; i < parent_a.size()/3; i++){
         child.push_back(parent_a[i]);
     }
 
-    for (unsigned i = 0; i < parent_b.size(); i++){
+    for (size_t i = 0; i < parent_b.size(); i++){
         // If the node isn't found in the child
         if(std::find(child.begin(), child.end(),parent_b[i]) == child.end()){
             child.push_back(parent_b[i]);
@@ -92,8 +89,8 @@ std::vector<Node> make_child(std::vector<Node> &parent_a, std::vector<Node> &par
 
 
 void Traverser::mutate(){
-    for (unsigned i = 0; i < this->new_generation.size(); i++){
-        for (unsigned k = 0; k < int(MUTATION_RATE * this->nodes.size()); k++){
+    for (size_t i = 0; i < this->new_generation.size(); i++){
+        for (size_t k = 0; k < int(MUTATION_RATE * this->nodes.size()); k++){
             int a = rand()%(this->nodes.size()-1);
             int b = rand()%(this->nodes.size()-1);
             Node tmpa = this->new_generation[i][a], tmpb = this->new_generation[i][b];
@@ -106,7 +103,7 @@ void Traverser::mutate(){
 void Traverser::copulate(std::vector<std::vector<std::vector<Node>>> &parents){
     std::vector<std::vector<Node>> temp_new_generation;
 
-    for (unsigned i = 0; i < parents[0].size(); i++){ // Since they are both the same size anyway.
+    for (size_t i = 0; i < parents[0].size(); i++){ // Since they are both the same size anyway.
         std::vector<Node> child = make_child(parents[0][i], parents[1][i]);
         temp_new_generation.push_back(child);
     }
@@ -116,7 +113,7 @@ void Traverser::copulate(std::vector<std::vector<std::vector<Node>>> &parents){
 
 std::pair<int,double> find_best_index_and_fitness(std::vector<double> &fitness_scores){
     std::pair<int,double> best_fitness = std::pair<int,double>(-1,INT_MAX);
-    for (unsigned i = 0; i < fitness_scores.size(); i++){
+    for (size_t i = 0; i < fitness_scores.size(); i++){
         if(fitness_scores[i] < best_fitness.second){
             best_fitness.second = fitness_scores[i];
             best_fitness.first = i;
@@ -124,7 +121,11 @@ std::pair<int,double> find_best_index_and_fitness(std::vector<double> &fitness_s
     }
     return best_fitness;
 }
-
+char* get_timestamp(){
+    time_t now = time(0);
+    char *dt = ctime(&now);
+    return dt; 
+}
 void Traverser::genetic_algorithm_driver(){
     int n = 0;
     this->initialize_nodes();
@@ -138,14 +139,19 @@ void Traverser::genetic_algorithm_driver(){
     
     while(true){
         n++;
+        if (n%100000 == 0){
+            n = 0;
+            std::cout << "we are alive and well.. " << get_timestamp() << std::endl;
+        }
         this->calculate_all_fitness(this->new_generation);   
         parents = this->select_parents(this->new_generation);
         this->copulate(parents);
         this->mutate();
         std::pair<int,double> best = find_best_index_and_fitness(this->fitness_scores);
         if(best.second < CURRENT_BEST){
+            
             CURRENT_BEST = best.second;
-            std::cout << "Found new shorter cycle " << CURRENT_BEST << "\n";
+            std::cout << "Found new shorter cycle " << CURRENT_BEST << " " << get_timestamp() << "\n";
             this->dump_cycle(best.second, this->new_generation[best.first]);
         }
     }
@@ -154,32 +160,29 @@ void Traverser::genetic_algorithm_driver(){
 
 void Traverser::calculate_all_fitness(std::vector<std::vector<Node>> &population){
     this->fitness_scores.clear();
+    double dist = 0;
 
-    for (unsigned i = 0; i < population.size(); i++){
-        this->distance_traversed = 0;
+    for (size_t i = 0; i < population.size(); i++){
+        dist = 0;
 
-        for(unsigned k = 0; k < population[i].size()-1; k++){ 
-            this->distance_traversed += dist_calc(population[i][k],population[i][k+1]);
+        for(size_t k = 0; k < population[i].size()-1; k++){ 
+            dist += dist_calc(population[i][k],population[i][k+1]);
         }
         // To add the fitness for the last node which is supposed to connect to the first node
-        this->distance_traversed += dist_calc(population[i][0],population[i][population[i].size()-1]);
-        this->fitness_scores.push_back(this->distance_traversed);
+        dist += dist_calc(population[i][0],population[i][population[i].size()-1]);
+        this->fitness_scores.push_back(dist);
     }
     
 }
 
 void Traverser::randomize_parents(){
     int index, cnt = 0;
-    // std::ofstream r;
-    // r.open(CYCLE_FILE, std::ofstream::out | std::ofstream::trunc); // To wipe the file for each new iteration.
-    // r.close();
+
     while (cnt < POPULATION_SIZE){
         cnt++;
         while (this->nodes.size() > 1){
             index = rand()%(this->nodes.size());
-            this->current_node = new Node(this->nodes[index]);
-            this->cycle.push_back(*this->current_node);
-            // delete this->current_node;
+            this->cycle.push_back(this->nodes[index]);
             this->nodes.erase(this->nodes.begin() + index);
         }
         this->parent_population.push_back(this->cycle);
@@ -192,12 +195,11 @@ void Traverser::initialize_nodes(){
     // We do this in case we are traversing from another starting node.
     this->nodes.clear();
     this->cycle.clear();
-    this->distance_traversed = 0;
     this->get_data();
 }
 
 void Traverser::get_data(){
-    std::ifstream data_file("../data16k.txt");
+    std::ifstream data_file(DATA16K);
     std::string line;
 
     while(getline(data_file,line)){
@@ -218,12 +220,6 @@ __always_inline void to_json(nlohmann::json &j, const Node &node) {
     j = node.serialize();
 }
 
-// __always_inline void from_json(nlohmann::json &j, Node &node){
-    //
-// }
-
-
-
 void Traverser::dump_cycle(double fitness, std::vector<Node> &cycle){
     std::string start_coord = "(" + std::to_string(cycle[0].x) + "," + std::to_string(cycle[0].y) + ")";
     nlohmann::json data;
@@ -231,62 +227,8 @@ void Traverser::dump_cycle(double fitness, std::vector<Node> &cycle){
     data["start_coord"] = start_coord;
     data["zcycle"] = cycle; // zcycle to order it to the back of the ndjson
     
-    // data[start_coord] = this->distance_traversed; // {this->distance_traversed, this->cycle};
     std::ofstream o, r;
-    o.open(CYCLE_FILE, std::ios_base::app);
+    o.open(CYCLE_FILE_16K, std::ios_base::app);
     o << data << std::endl;
     o.close();
-}
-
-
-
-void Traverser::close_cycle(){
-    this->cycle.push_back(this->cycle[0]);
-    this->distance_traversed += dist_calc(*this->current_node, this->cycle[0]);
-    std::cout << "cycle len: " << this->cycle.size() << std::endl;
-    std::cout << "distance traversed: " << this->distance_traversed << std::endl;
-    // this->dump_cycle();
-    // this->reset_traversal();
-    // exit(EXIT_SUCCESS);
-}
-
-
-
-std::vector<std::pair<size_t,size_t>> Traverser::calc_clusters_for_threads(){
-    /*
-    Calculates the size of the clusters for each thread.
-    Returns a vector containing the start index and the stopping index
-    for operations on this->nodes.
-    */
-    std::vector<std::pair<size_t,size_t>> vals = {};
-    size_t size = this->nodes.size();
-
-    if(size > 1000){
-        NUM_THREADS = size / CLUSTER_SIZE;
-        NUM_THREADS++;
-        size_t length = size / NUM_THREADS;
-        size_t remainder = size % NUM_THREADS;
-        size_t chunk_a = length;
-        size_t a_start = 0, a_stop = 0, b_start = 0, b_stop = 0;
-
-        for (size_t i = 0; i < NUM_THREADS; i++){
-            if(i < NUM_THREADS - remainder){
-                a_start = ((i*chunk_a));
-                a_stop = (chunk_a*(i+1))-1; // -1 due to indexing.
-                vals.push_back(std::pair<size_t,size_t>(a_start,a_stop));
-
-            }
-
-            if(remainder && i >= NUM_THREADS-remainder){
-                b_start = a_stop + (i%(NUM_THREADS-remainder))*(chunk_a+1)+1;
-                b_stop = a_stop + ((i%(NUM_THREADS-remainder))+1)*(chunk_a+1);
-                vals.push_back(std::pair<size_t,size_t>(b_start,b_stop));
-            }
-        }
-        return vals;
-    }
-    else{
-        vals.push_back(std::pair<size_t,size_t>(0,size-1));
-        return vals;
-    }
 }
